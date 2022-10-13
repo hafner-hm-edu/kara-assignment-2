27fd51210bf1b9e88c7eb1b207217c676eb715bb
Ulli Hafner <ullrich.hafner@gmail.com>
2022-10-13 23:16:18 +0200

    Prepare files for the second assignment.

--- src/main/java/edu/hm/hafner/kara/Assignment05.java
+++ src/main/java/edu/hm/hafner/kara/Assignment05.java
@@ -3,88 +3,45 @@ package edu.hm.hafner.kara;
 import static de.i8k.karalight.Kara.*;
 
 /**
- * KaraLight: solution of assignment 5.
+ * KaraLight: Template für die Übungsaufgaben.
  *
  * @author Ullrich Hafner
  */
 public class Assignment05 {
     /**
-     * Entrypoint for Kara: this method is called once if you press the 'Ausführen' button in KaraLight.
+     * Die {@code main} Methode ist der Ausgangspunkt für KaraLight. Hier wird direkt in Java programmiert, folgende
+     * Kara-Befehle können verwendet werden, um Kara zu steuern:
+     * <ul>
+     *   <li>{@code move()} - Kara bewegt sich einen Schritt nach vorn. Das geht nur, wenn vor Kara kein Baum ist!
+     *   Wenn vor Kara ein Pilz ist, schiebt Kara den Pilz eine Position weiter.
+     *   (Das setzt wiederum voraus, dass der Platz vor dem Pilz frei ist). </li>
+     *   <li>{@code turnRight()} bzw. {@code turnLeft()} - Kara dreht sich nach rechts bzw. links</li>
+     *   <li>{@code pickLeaf()} - Kara nimmt ein Blatt auf (geht nur, wenn eins da ist!)</li>
+     *   <li>{@code putLeaf()} - Kara legt ein Blatt ab (geht nur, wenn keins da ist!)</li>
+     *   <li>{@code say(...)} - Kara gibt einen Text in einem Fenster aus.</li>
+     *   <li>{@code askNumber(...)} - Kara fragt nach einer Zahl, die den Ablauf des Programms variabel gestaltet.</li>
+     * </ul>
+     * Zusätzlich stehen Ihnen die folgenden Abfragen zur Verfügung:
+     * <ul>
+     *   <li>{@code isMushroomInFront()} - liefert {@code true}, wenn vor Kara ein Pilz steht</li>
+     *   <li>{@code isTreeInFront()} - liefert {@code true}, wenn vor Kara ein Baum steht</li>
+     *   <li>{@code isTreeLeft()} - liefert {@code true}, wenn links von Kara ein Baum steht</li>
+     *   <li>{@code isTreeRight()} - liefert {@code true}, wenn rechts von Kara ein Baum steht</li>
+     *   <li>{@code isOnLeaf()} - liefert {@code true}, wenn Kara auf einem Blatt steht</li>
+     * </ul>
      *
      * @param unused
-     *         this parameter is not used by KaraLight
+     *         Dieser Parameter wird von Kara nicht benutzt, muss aber bestehen bleiben, damit die KaraLight
+     *         Oberfläche in der Entwicklungsumgebung gestartet werden kann. Dieser Parameter ist auch erforderlich,
+     *         damit die automatisierte Auswertung der Ergebnisse funktioniert.
      */
     public static void main(final String... unused) {
-        drawLineWith4Points();
-        drawLineWith6Points();
-        drawMiddleLine();
-        drawLineWith4Points();
-        drawLineWith4Points();
-    }
-
-    /**
-     * Moves one row down.
-     */
-    protected static void down() {
-        turnRight();
-        move();
-        turnLeft();
-    }
-
-    private static void drawLineWith4Points() {
-        draw2Leaves();
-        pickLeafAndMove();
-        draw2Leaves();
-        down();
-    }
-
-    protected static void pickLeafAndMove() {
+        // Beispielcode, kann entfernt werden
         if (isOnLeaf()) {
             pickLeaf();
         }
-        move();
-    }
-
-    private static void draw2Leaves() {
-        drawLeafAndMove();
-        pickLeafAndMove();
-        pickLeafAndMove();
-        pickLeafAndMove();
-        drawLeafAndMove();
-    }
-
-    protected static void drawLeafAndMove() {
-        if (!isOnLeaf()) {
+        else {
             putLeaf();
         }
-        move();
-    }
-
-    private static void drawLineWith6Points() {
-        draw2Leaves();
-        pickLeafAndMove();
-        drawLeafAndMove();
-        drawLeafAndMove();
-        pickLeafAndMove();
-        drawLeafAndMove();
-        drawLeafAndMove();
-
-        down();
-    }
-
-    private static void drawMiddleLine() {
-        drawLeafAndMove();
-        drawLeafAndMove();
-        drawLeafAndMove();
-        drawLeafAndMove();
-        drawLeafAndMove();
-        pickLeafAndMove();
-        drawLeafAndMove();
-        pickLeafAndMove();
-        drawLeafAndMove();
-        pickLeafAndMove();
-        drawLeafAndMove();
-
-        down();
     }
 }
--- src/main/java/edu/hm/hafner/kara/Assignment07.java
+++ src/main/java/edu/hm/hafner/kara/Assignment07.java
@@ -3,51 +3,45 @@ package edu.hm.hafner.kara;
 import static de.i8k.karalight.Kara.*;
 
 /**
- * KaraLight: solution of assignment 7.
+ * KaraLight: Template für die Übungsaufgaben.
  *
  * @author Ullrich Hafner
  */
 public class Assignment07 {
     /**
-     * Entrypoint for Kara: this method is called once if you press the 'Ausführen' button in KaraLight.
+     * Die {@code main} Methode ist der Ausgangspunkt für KaraLight. Hier wird direkt in Java programmiert, folgende
+     * Kara-Befehle können verwendet werden, um Kara zu steuern:
+     * <ul>
+     *   <li>{@code move()} - Kara bewegt sich einen Schritt nach vorn. Das geht nur, wenn vor Kara kein Baum ist!
+     *   Wenn vor Kara ein Pilz ist, schiebt Kara den Pilz eine Position weiter.
+     *   (Das setzt wiederum voraus, dass der Platz vor dem Pilz frei ist). </li>
+     *   <li>{@code turnRight()} bzw. {@code turnLeft()} - Kara dreht sich nach rechts bzw. links</li>
+     *   <li>{@code pickLeaf()} - Kara nimmt ein Blatt auf (geht nur, wenn eins da ist!)</li>
+     *   <li>{@code putLeaf()} - Kara legt ein Blatt ab (geht nur, wenn keins da ist!)</li>
+     *   <li>{@code say(...)} - Kara gibt einen Text in einem Fenster aus.</li>
+     *   <li>{@code askNumber(...)} - Kara fragt nach einer Zahl, die den Ablauf des Programms variabel gestaltet.</li>
+     * </ul>
+     * Zusätzlich stehen Ihnen die folgenden Abfragen zur Verfügung:
+     * <ul>
+     *   <li>{@code isMushroomInFront()} - liefert {@code true}, wenn vor Kara ein Pilz steht</li>
+     *   <li>{@code isTreeInFront()} - liefert {@code true}, wenn vor Kara ein Baum steht</li>
+     *   <li>{@code isTreeLeft()} - liefert {@code true}, wenn links von Kara ein Baum steht</li>
+     *   <li>{@code isTreeRight()} - liefert {@code true}, wenn rechts von Kara ein Baum steht</li>
+     *   <li>{@code isOnLeaf()} - liefert {@code true}, wenn Kara auf einem Blatt steht</li>
+     * </ul>
      *
      * @param unused
-     *         this parameter is not used by KaraLight
+     *         Dieser Parameter wird von Kara nicht benutzt, muss aber bestehen bleiben, damit die KaraLight
+     *         Oberfläche in der Entwicklungsumgebung gestartet werden kann. Dieser Parameter ist auch erforderlich,
+     *         damit die automatisierte Auswertung der Ergebnisse funktioniert.
      */
     public static void main(final String... unused) {
-        drawSideAndMoveToNextSide();
-        drawSideAndMoveToNextSide();
-        drawSideAndMoveToNextSide();
-        drawSideAndMoveToNextSide();
-
-        moveToOrigin();
-    }
-
-    private static void moveToOrigin() {
-        turnAround();
-        move();
-        turnRight();
-        move();
-        turnRight();
-    }
-
-    private static void drawSideAndMoveToNextSide() {
-        drawLine();
-        turnAround();
-        move();
-        turnLeft();
-        move();
-    }
-
-    private static void turnAround() {
-        turnRight();
-        turnRight();
-    }
-
-    private static void drawLine() {
-        while (!isOnLeaf()) {
+        // Beispielcode, kann entfernt werden
+        if (isOnLeaf()) {
+            pickLeaf();
+        }
+        else {
             putLeaf();
-            move();
         }
     }
 }
